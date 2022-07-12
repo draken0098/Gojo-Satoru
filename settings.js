e20257f74aac55759a849d6a1f1284aa98121c09
kemodraken <109114857+draken0098@users.noreply.github.com>
2022-07-12 21:01:25 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = [+201016865749]
+global.premium = [+201016865749]
+global.ownernomer = +201016865749
+global.ownername = 𝐊𝐄𝐌𝐎
+global.botname = 𝐃𝐑𝐀𝐊𝐄𝐍
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
